3beaef669bd9cf079d3bf56f2247b6d98b16adf1
wanda <rashiidahweira@gmail.com>
2023-12-07 15:17:28 +0300

    Clean up

--- src/components/ExploreArtists.jsx
+++ src/components/ExploreArtists.jsx
@@ -2,7 +2,7 @@ import GridItem from "./GridItem";
 
 const ExploreArtists = () => {
   return (
-    <div className="my-28">
+    <div className="my-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-4 sm:mx-10 md:mx-28 my-8">
         <GridItem />
         <GridItem />
